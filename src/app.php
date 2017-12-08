e8c71fa52e5827811d89d493142c6b11af5b6f8f
unknown <wseghoua@student.42.fr>
2017-12-08 14:58:06 +0100

    first commit

---
+++ src/app.php
@@ -0,0 +1,110 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use App\CustomApp;
+
+$app = new CustomApp();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+$app['users.dao'] = function($app) {
+    return new \DAO\UserDAO($app['pdo']);
+};
+
+$app['categories.dao'] = function($app) {
+    return new \DAO\CategoryDAO($app['pdo']);
+};
+
+$app['loaning.dao'] = function($app) {
+    return new \DAO\LoaningDAO($app['pdo']);
+};
+
+$app['games.dao'] = function($app) {
+    return new \DAO\GameDAO($app['pdo']);
+};
+
+$app['pdo'] = function($app) {
+    $options = $app['pdo.options'];
+    return new \PDO("{$options['dbms']}://host={$options['host']};dbname={$options['dbname']};charset={$options['charset']}",
+            $options['username'], 
+            $options['password'], 
+            array(
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+            ));
+};
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => [
+        'admin' => array(
+            'pattern' => '^/admin/',
+            'form' => array(
+                'login_path' => '/loginadmin', 
+                'check_path' => '/admin/login_check',
+                'always_use_default_target_path' => true,
+                'default_target_path' => '/admin/dashboard'
+            ),
+//            'http' => true,
+            'anonymous' => false,
+            'logout' => array('logout_path' => '/admin/logoutadmin', 'invalidate_session' => true),
+            'users' => function () use ($app) {
+                return $app['admins.dao'];
+            },
+            //https://symfony.com/doc/current/security/form_login.html
+//            'form_login' => array(
+//                'default_target_path' => 'admin_dashboard',
+//            ),
+        ),
+        'front' => array(
+            'pattern' => '^/',
+            'http' => true,
+            'anonymous' => true,
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'logout' => array('logout_path' => '/logout', 'invalidate_session' => true),
+            'users' => function () use ($app) {
+                return $app['users.dao'];
+            }
+        ),
+    ]
+));
+        
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('fr'),
+    'translator.domains' => [
+        // http://sharemycode.fr/2yd
+        'messages' => [
+            'fr' => [
+                'The credentials were changed from another session.' => 'Les identifiants ont été changés dans une autre session.',
+                'The presented password cannot be empty.' => 'Le mot de passe ne peut pas être vide.',
+                'The presented password is invalid.' => 'Le mot de passe entré est invalide.',
+                'Bad credentials.' => 'Les identifiants sont incorrects'
+            ]
+        ]
+    ]
+));
+
+$app->register(new FormServiceProvider());
+$app->register(new ValidatorServiceProvider());
+
+$app['admins.dao'] = function ($app) {
+    return new DAO\AdminDAO($app['pdo']);
+};
+
+return $app;
---
+++ src/controllers.php
@@ -0,0 +1,97 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+require __DIR__ . '/controllers_admin.php';
+
+$app->before(function() use ($app) {
+    $token = $app['security.token_storage']->getToken();
+
+    if ($token) {
+        $user = $token->getUser();
+    } else {
+        $user = null;
+    }
+
+    if (is_string($user)) {
+        $user = null;
+    }
+
+    $app['user'] = $user;
+});
+
+$app->get('/', function () use ($app) {
+
+            return $app['twig']->render('index.html.twig');
+        })
+        ->bind('homepage')
+;
+
+$app->get('/login', function(Request $request) use ($app) {
+            return $app['twig']->render('login.html.twig', array(
+                        'error' => $app['security.last_error']($request),
+                        'last_username' => $app['session']->get('_security.last_username'),
+            ));
+        })
+        ->bind('login')
+;
+
+$app->match('/register', function(Request $request) use ($app) {
+            $user = new \Entity\User();
+
+            $form = $app['form.factory']->createBuilder(\FormType\UserType::class, $user, [
+                        'validation_groups' => ['registration']
+                    ])
+                    ->add('submit', SubmitType::class, [
+                        'label' => 'Envoyer'
+                    ])
+                    ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $user->setRole('ROLE_USER');
+
+                $salt = md5(time());
+
+                $user->setSalt($salt);
+                $encodedPassword = $app['security.encoder_factory']
+                        ->getEncoder($user)
+                        ->encodePassword($user->getPassword(), $user->getSalt());
+
+                $user->setPassword($encodedPassword);
+
+                $app['users.dao']->save($user);
+
+                return $app->redirect($app['url_generator']->generate('login'));
+            }
+
+            $formView = $form->createView();
+
+            return $app['twig']->render('register.html.twig', ['form' => $formView]);
+        })
+        ->method('GET|POST')
+        ->bind('register')
+;
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/' . $code . '.html.twig',
+        'errors/' . substr($code, 0, 2) . 'x.html.twig',
+        'errors/' . substr($code, 0, 1) . 'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
